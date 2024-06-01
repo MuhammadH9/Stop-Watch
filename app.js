a189117fc899bcdc0959a35e1cf30298036601f6
muhammadh9 <m.huzaif999@gmail.com>
2024-06-02 01:12:21 +0500

    Stopwatch

---
+++ app.js
@@ -0,0 +1,62 @@
+var hours = document.getElementById("hours")
+var minutes = document.getElementById("minutes")
+var seconds = document.getElementById("seconds")
+var mseconds = document.getElementById("mseconds")
+var interval;
+
+var hr = 0
+var min = 0
+var sec = 0
+var msec = 0
+
+function start() {
+    msec++
+    mseconds.innerHTML = msec
+
+    if (msec === 100) {
+        sec++
+        seconds.innerHTML = sec
+        msec = 0
+
+
+    } else if (sec > 59) {
+        min++
+        minutes.innerHTML = min
+        sec = 0
+    } else if (min > 59) {
+        hr++
+        hours.innerHTML = hr
+        min = 0
+    }
+
+
+}
+
+
+
+function startWatch() {
+    interval = setInterval(start, 10)
+    alert("Start")
+}
+
+
+function resetWatch() {
+    clearInterval(interval)
+    alert("Reset")
+    hours.innerHTML = "00"
+    minutes.innerHTML = "00"
+    seconds.innerHTML = "00"
+    mseconds.innerHTML = "00"
+    hr = 0
+    min = 0
+    sec = 0
+    msec = 0
+}
+
+
+
+
+function stopWatch() {
+    clearInterval(interval)
+        alert("Stop")
+}
